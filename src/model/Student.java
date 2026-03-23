52dfa6bf17a4a6d2a1264ee373309656cd07b75b
Ritika Gupta <guptaritikagupta2005@gmail.com>
2026-03-23 22:59:30 +0530

    Add attendance fields and methods in Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,8 +1,29 @@
 package model;
 
 public class Student extends Person {
+    private int totalClasses;
+    private int presentCount;
 
     public Student(String id, String name) {
         super(id, name);
+        this.totalClasses = 0;
+        this.presentCount = 0;
+    }
+
+    public int getTotalClasses() {
+        return totalClasses;
+    }
+
+    public int getPresentCount() {
+        return presentCount;
+    }
+
+    public void markPresent() {
+        this.totalClasses++;
+        this.presentCount++;
+    }
+
+    public void markAbsent() {
+        this.totalClasses++;
     }
 }
