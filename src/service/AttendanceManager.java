b4d06a52a529d275c371a66ea820bf6639f8c054
Ritika Gupta <guptaritikagupta2005@gmail.com>
2026-03-26 23:03:53 +0530

    Create AttendanceManager and manage student collection using ArrayList

--- src/service/AttendanceManager.java
+++ src/service/AttendanceManager.java
@@ -1,4 +1,21 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Student;
+
 public class AttendanceManager {
+    private List<Student> students;
+
+    public AttendanceManager() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return this.students;
+    }
 }
